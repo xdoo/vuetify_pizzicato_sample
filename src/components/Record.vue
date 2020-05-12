47e264336189c557d514156f0dd16ffde68893c7
xdoo <claus.straube@gmail.com>
2020-05-12 15:00:51 +0200

    minor changes

---
+++ src/components/Record.vue
@@ -0,0 +1,22 @@
+<template>
+ <div>
+  <v-btn>Start recording</v-btn>  
+ </div> 
+</template>
+<script lang="ts">
+import Vue from "vue"
+import { Component } from "vue-property-decorator"
+
+// @ts-ignore
+import PizzicatoRecorder from "pizzicato-recorder"
+// @ts-ignore
+import Pizzicato from "pizzicato"
+
+@Component
+export default class Record extends Vue{
+
+  record() {
+    PizzicatoRecorder(Pizzicato)
+  }
+}
+</script>
